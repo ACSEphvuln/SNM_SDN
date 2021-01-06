f011d9b0ac1fd84c1b53bbcb973558ed013bb1ea
None <None>
2021-01-06 05:32:52 -0800

    Added openflow listen ports to routers

--- topology.py
+++ topology.py
@@ -13,12 +13,12 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Add routers and firewalls
-        r1 = self.addSwitch('r1',dpid='000000000100')
-        r2 = self.addSwitch('r2',dpid='000000000200')
-        r3 = self.addSwitch('r3',dpid='000000000300')
-        r4 = self.addSwitch('r4',dpid='000000000400')
-        f1 = self.addSwitch('f1',dpid='00000000f100')
-        f2 = self.addSwitch('f2',dpid='00000000f200')
+        r1 = self.addSwitch('r1',dpid='000000000100', listenPort='6634')
+        r2 = self.addSwitch('r2',dpid='000000000200', listenPort='6635')
+        r3 = self.addSwitch('r3',dpid='000000000300', listenPort='6636')
+        r4 = self.addSwitch('r4',dpid='000000000400', listenPort='6637')
+        f1 = self.addSwitch('f1',dpid='00000000f100', listenPort='6638')
+        f2 = self.addSwitch('f2',dpid='00000000f200', listenPort='6639')
 
         # Add links between routers & firewalls
         self.addLink(r1, r2, port1=1, port2=1, intfName1='r1-eth1', intfName2='r2-eth1')
